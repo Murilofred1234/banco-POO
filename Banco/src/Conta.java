3da1428b7a632cced12ef3f478db6c686af59abc
Murilo <murilofrederico18@gmail.com>
2024-05-22 16:24:23 -0300

    intercaes e logica funcionando

---
+++ Banco/src/Conta.java
@@ -0,0 +1,54 @@
+public abstract class Conta implements IConta {
+
+    protected static int AGENCIA_PADRAO = 1;
+    protected static int SEQUENCIAL = 1;
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+
+    public Conta(){
+        this.agencia = Conta.AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+    }
+
+    @Override
+    public void sacar(double valor) {
+        saldo = saldo - valor;
+    }
+    @Override
+    public void depositar(double valor) {
+        saldo = saldo + valor;
+    }
+    @Override
+    public void transferir(double valor, Conta contaDestino) {
+        this.sacar(valor);
+        contaDestino.depositar(valor);
+    }
+
+    public void sacar(){
+        
+    }
+    public void depositar(){
+        
+    }
+    public void sactransferir(){
+        
+    }
+    public int getAgencia() {
+        return agencia;
+    }
+    public int getNumero() {
+        return numero;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    protected void imprimirInformacoes() {
+        System.out.println(String.format("Agencia %d", this.agencia));
+        System.out.println(String.format("Número %d", this.numero));
+        System.out.println(String.format("Saldo %.2f", this.saldo));
+    }
+
+    
+
+}
